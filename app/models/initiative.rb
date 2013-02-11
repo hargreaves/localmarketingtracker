19b8ef9f4eefda818474627f6b39f81f8d1117f8
hargreaves <brad.hargreaves@gmail.com>
2013-02-11 18:55:45 -0500

    added validations and some cleanup

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -2,4 +2,10 @@ class Initiative < ActiveRecord::Base
   attr_accessible :actualemails, :budget, :enddate, :landingpage, :launchdate, :name, :offer, :spent, :target, :targetemails, :teamlead, :market_id
   
   belongs_to :market
+  
+  validates :name, :presence => true
+  validates :teamlead, :presence => true
+  validates :targetemails, :presence => true
+  validates :budget, :presence => true
+  
 end
